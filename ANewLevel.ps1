5443dd76b3110cba1103f3b7f04a6fac107ec3e0
XelaNosnibor <alex.d.robinson95@gmail.com>
2021-05-11 21:21:08 +1000

    added files via upload

---
+++ ANewLevel.ps1
@@ -0,0 +1,37 @@
+﻿cls
+
+
+$E=329.6  # 0
+$F=349.2  # 1
+$Fs=370.0 # 2
+$G=392.0  # 3
+$Gs=415.3 # 4
+$A=440    # 5
+$As=466.1 # 6
+$B=493.9  # 7
+
+$tempo=230 # In Millisecs
+
+while($true)
+{
+    [console]::beep($E,$tempo) 
+    [console]::beep($F,$tempo)
+    [console]::beep($Fs,$tempo)
+    [console]::beep($E,$tempo) 
+    [console]::beep($F,$tempo)
+    [console]::beep($Fs,$tempo)
+    [console]::beep($G,$tempo)
+    [console]::beep($E,$tempo) 
+
+    [console]::beep($F,$tempo)
+    [console]::beep($Fs,$tempo)
+    [console]::beep($G,$tempo) 
+    [console]::beep($Gs,$tempo)
+    [console]::beep($As,$tempo)
+    [console]::beep($As,$tempo)
+    [console]::beep($As,$tempo)
+    [console]::beep($As,$tempo)
+}
+
+
+
---
+++ ThunderstruckRiff.ps1
@@ -0,0 +1,85 @@
+﻿$B3=246.94  # 0  [console]::beep($B3,$tempo)
+$Ds4=311.13 # 4  [console]::beep($Ds4,$tempo)
+$E4=329.63  # 5  [console]::beep($E4,$tempo)
+$Fs4=369.99 # 7  [console]::beep($Fs4,$tempo)
+$G4=392.00  # 8  [console]::beep($G4,$tempo)
+$Gs4=415.30 # 9  [console]::beep($sG4,$tempo)
+$A4=440.00  # 10 [console]::beep($A4,$tempo)
+$B4=493.88  # 12 [console]::beep($B4,$tempo)
+
+$tempo=200 #0.2 sec
+$tempo1=150 #0.2 sec
+
+
+while($true)
+{
+# 4 0 7 0 4 0 7 0 4 0 7 0 4 0 7 0
+    [console]::beep($Ds4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($Fs4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($Ds4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($Fs4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($Ds4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($Fs4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($Ds4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($Fs4,$tempo)
+    [console]::beep($B3,$tempo)
+
+# 5 0 8 0 5 0 8 0 5 0 8 0 5 0 8 0
+    [console]::beep($E4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($G4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($E4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($G4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($E4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($G4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($E4,$tempo)
+    [console]::beep($B3,$tempo)
+    [console]::beep($G4,$tempo)
+    [console]::beep($B3,$tempo)
+
+# 12 0 10 0| 9 0 10 0| 9 0 7 0| 9 0 5 0| 7 0 4 0| 5 0 4 0| 5 0 4 0| 5 0 4 0
+    [console]::beep($B4,$tempo1)
+    [console]::beep($B3,$tempo1)
+    [console]::beep($A4,$tempo1)
+    [console]::beep($B3,$tempo1) # 12 0 10 0
+    [console]::beep($Gs4,$tempo1)
+    [console]::beep($B3,$tempo1)
+    [console]::beep($A4,$tempo1)
+    [console]::beep($B3,$tempo1) # 9 0 10 0
+    [console]::beep($Gs4,$tempo1)
+    [console]::beep($B3,$tempo1)
+    [console]::beep($Fs4,$tempo1)
+    [console]::beep($B3,$tempo1) # 9 0 7 0
+    [console]::beep($Gs4,$tempo1)
+    [console]::beep($B3,$tempo1)
+    [console]::beep($E4,$tempo1)
+    [console]::beep($B3,$tempo1) # 9 0 5 0
+    [console]::beep($Fs4,$tempo1)
+    [console]::beep($B3,$tempo1)
+    [console]::beep($E4,$tempo1)
+    [console]::beep($B3,$tempo1) # 7 0 4 0
+    [console]::beep($E4,$tempo1)
+    [console]::beep($B3,$tempo1)
+    [console]::beep($Ds4,$tempo1)
+    [console]::beep($B3,$tempo1) # 5 0 4 0
+    [console]::beep($E4,$tempo1)
+    [console]::beep($B3,$tempo1)
+    [console]::beep($Ds4,$tempo1)
+    [console]::beep($B3,$tempo1) # 5 0 4 0
+    [console]::beep($E4,$tempo1)
+    [console]::beep($B3,$tempo1)
+    [console]::beep($Ds4,$tempo1)
+    [console]::beep($B3,$tempo1) # 5 0 4 0
+}
